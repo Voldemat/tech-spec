e387e4f6d4869bb97597fbee781f9ff9fd017371
Владимир Войтенко <vladimirdev635@gmail.com>
2023-07-05 00:26:13 +0500

    fix

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
+import path from 'path'
 import { Command } from 'commander'
 import { validate } from './actions/validate';
 
@@ -9,6 +10,7 @@ program
     .description('Validates yaml spec against schema')
     .argument('<path-to-dir>', 'Path to directory containing yaml spec files')
     .action((pathToDir: string, _) => {
+        pathToDir = path.join(process.env.PWD!, pathToDir)
         const result = validate(pathToDir);
         if (result.isError) {
             program.error(result.message)
