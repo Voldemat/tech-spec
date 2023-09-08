6000e33d6c9f2cce388d61c8027ee89866af2545
Vladimir Voitenko <vladimirdev635@gmail.com>
2023-09-08 19:18:45 +0500

    packages:js:update-schema

--- packages/js/src/spec/schemas.ts
+++ packages/js/src/spec/schemas.ts
@@ -1,8 +1,8 @@
 import mainSchema from '../schema/main.json'
-import formSchema from '../schema/form.json'
-import featureSchema from '../schema/feature.json'
-import designSystemSchema from '../schema/designSystem.json'
-import typeSchema from '../schema/type.json'
+import formSchema from '../schema/spec-types/form.json'
+import featureSchema from '../schema/spec-types/feature.json'
+import designSystemSchema from '../schema/spec-types/designSystem.json'
+import typeSchema from '../schema/spec-types/type.json'
 
 export {
     mainSchema,
